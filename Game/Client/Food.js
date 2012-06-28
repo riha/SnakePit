3d153aeaabb8de679ba629ac4f7ab8fcbca09708
Richard Hallgren <richard.hallgren@gmail.com>
2012-06-28 20:56:59 +0200

    Adding of initial files to handle snake food

--- Game/Client/Food.js
+++ Game/Client/Food.js
@@ -1,38 +1,44 @@
-﻿var Food = function (context, snake) {
+﻿var Food = function (context, position) {
     var self = this;
     var thickness = 8;
     var lastDraw = undefined;
     var request;
     var degrees = 0;
 
-    function clear() {
-        if (self.position != undefined) {
-            context.save();
-            context.translate(self.position.x + thickness, self.position.y + thickness);
-            context.clearRect(-9, -9, thickness * 2 + 3, thickness * 2 + 3);
-            context.restore();
-        };
-    };
+    var position = position;
 
-    function spin() {
-        if (lastDraw === undefined || Date.now() - lastDraw > 50) {
-            degrees += 0.200;
-            clear();
-            draw(degrees);
-            //console.log("spin food");
-            lastDraw = Date.now();
-        };
+    //    function clear() {
+    //        if (self.position != undefined) {
+    //            console.log("clear;");
+    //            context.save();
+    //            context.translate(self.position.x + thickness, self.position.y + thickness);
+    //            context.clearRect(-9, -9, thickness * 2 + 3, thickness * 2 + 3);
+    //            context.restore();
+    //        };
+    //    };
 
-        request = requestAnimFrame(spin);
-    };
+    //    function spin() {
+    //        console.log("enter spin", lastDraw)
+    //        if (lastDraw == undefined || Date.now() - lastDraw > 50) {
+    //            //console.log("spin food 1");
+    //            degrees += 0.200;
+    //            clear();
+    //            draw(degrees);
+    //            //console.log("spin food 2");
+    //            lastDraw = Date.now();
+    //        };
+
+    //        request = requestAnimFrame(spin);
+    //    };
 
     function draw(rotation) {
-        if (rotation === undefined)
+        if (rotation == undefined)
             rotation = 0;
+        console.log("draw");
 
         context.save();
-        context.translate(self.position.x + thickness, self.position.y + thickness);
-        context.rotate(rotation);
+        context.translate(position.toFixed().x + thickness, position.toFixed().y + thickness);
+        //context.rotate(rotation);
         context.fillStyle = "#000";
 
         context.shadowOffsetX = 2;
@@ -43,29 +49,30 @@
         roundRect(context, -4, -8, thickness, thickness * 2, 3, true, false);
 
         context.fillStyle = "#AACC99";
+
         context.strokeStyle = "#000";
         roundRect(context, -2.5, -2.5, 5, 5, 2, true, false);
         context.restore();
     };
 
-    function calculatePosition() {
-        var position = new Point(Math.round(Math.random() * 490 / 20) * 20, Math.round(Math.random() * 490 / 20) * 20);
-        for (var i = 0; i < snake.parts.length - 1; i++) {
-            var part = snake.parts[i];
-            if (part.equal(position)) {
-                position = undefined;
-                break;
-            };
-        };
+    //    function calculatePosition() {
+    //        var position = new Point(Math.round(Math.random() * 490 / 20) * 20, Math.round(Math.random() * 490 / 20) * 20);
+    //        for (var i = 0; i < snake.parts.length - 1; i++) {
+    //            var part = snake.parts[i];
+    //            if (part.equal(position)) {
+    //                position = undefined;
+    //                break;
+    //            };
+    //        };
 
-        return position;
-    };
+    //        return position;
+    //    };
 
-    clear();
-    
-    do {
-        self.position = calculatePosition();
-    } while (self.position == undefined);
+    // clear();
 
-    spin();
+    //    do {
+    //        self.position = calculatePosition();
+    //    } while (self.position == undefined);
+
+    draw();
 };
---
+++ Game/Server/FoodStore.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SnakePit.Game.Server
+{
+    public static class FoodStore
+    {
+        public static readonly Dictionary<string, Point> test = new Dictionary<string, Point>();
+
+        public static void AddFood(string key, Point p)
+        {
+            if (test.ContainsKey(key))
+            {
+                test[key] = p;
+            }
+            else
+            {
+                test.Add(key, p);
+            }
+        }
+    }
+}
